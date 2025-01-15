5bd8581d150f01523bf5fc72aa5d6a694cd8fd64
Arthur Lopes <arthurlopr12@gmail.com>
2025-01-14 21:27:45 -0300

    initial commit

---
+++ src/main/java/com/study/grahqljava/GrahqlJavaApplication.java
@@ -0,0 +1,13 @@
+package com.study.grahqljava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrahqlJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrahqlJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/grahqljava/GrahqlJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.grahqljava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrahqlJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
